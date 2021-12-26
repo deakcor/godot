6d448f36fdda5a5ad056436e988d2ff9882837c1
Vincent D <40196601+deakcor@users.noreply.github.com>
2021-12-26 15:45:18 +0100

    add clipboard fcts in x11 and osx

--- platform/x11/os_x11.h
+++ platform/x11/os_x11.h
@@ -276,10 +276,13 @@ public:
 
 	virtual bool can_draw() const;
 
+	virtual bool has_text_clipboard() const;
+	virtual bool has_image_clipboard() const;
 	virtual void set_clipboard(const String &p_text);
 	virtual String get_clipboard() const;
 	virtual void set_clipboard_primary(const String &p_text);
 	virtual String get_clipboard_primary() const;
+	virtual Ref<Image> get_image_clipboard() const;
 
 	virtual void release_rendering_thread();
 	virtual void make_rendering_thread();
